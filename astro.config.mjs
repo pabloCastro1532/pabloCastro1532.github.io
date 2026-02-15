7c2561c891f60cbb4108cd856541f1e56b2ee523
Pablo <167651476+pabloCastro1532@users.noreply.github.com>
2026-02-15 17:12:02 -0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,5 +12,4 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
     integrations: [react(), tailwind(), sitemap()],
     site: 'https://pabloCastro1532.github.io',
-    base: '/',
 });
